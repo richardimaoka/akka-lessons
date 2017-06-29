2d070e066a211af8c37368a60b9dd89d2dc07d09
Richard Imaoka <richard.e.imaoka@gmail.com>
2017-06-29 18:44:29 +0900

    Graph DSL in the middle

--- src/main/scala/my/stream/GraphDSLbuilderTest.scala
+++ src/main/scala/my/stream/GraphDSLbuilderTest.scala
@@ -2,9 +2,13 @@ package my.stream
 
 import akka.NotUsed
 import akka.actor.ActorSystem
-import akka.stream.{ActorMaterializer, ClosedShape}
-import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Merge, RunnableGraph, Sink, Source}
+import akka.stream._
+import akka.stream.scaladsl.{Broadcast, Concat, Flow, GraphDSL, Merge, RunnableGraph, Sink, Source, Zip, ZipWith}
+import akka.stream.testkit.TestSubscriber
 import my.wrapper.Wrapper
+
+import scala.concurrent.{Await, Future, Promise}
+import scala.concurrent.duration._
 
 object GraphDSLbuilderTest {
   implicit val system = ActorSystem()
@@ -14,13 +18,17 @@ object GraphDSLbuilderTest {
     val g = RunnableGraph.fromGraph(GraphDSL.create() { implicit builder: GraphDSL.Builder[NotUsed] =>
       import GraphDSL.Implicits._
       val in = Source(1 to 10)
-      val out = Sink.ignore
+      val out = Sink.foreach[Int](println(_))
 
       val bcast = builder.add(Broadcast[Int](2))
       val merge = builder.add(Merge[Int](2))
 
       val f1, f2, f3, f4 = Flow[Int].map(_ + 10)
 
+      /**
+       * in ~> f1 ~> bcast ~> f2 ~> merge ~> f3 ~> out
+       *             bcast ~> f4 ~> merge
+       */
       in ~> f1 ~> bcast ~> f2 ~> merge ~> f3 ~> out
       bcast ~> f4 ~> merge
       ClosedShape
@@ -29,9 +37,110 @@ object GraphDSLbuilderTest {
     g.run()
   }
 
+  def testPartial(): Unit = {
+    val pickMaxOfThree = GraphDSL.create() { implicit b =>
+      import GraphDSL.Implicits._
+
+      val zip1 = b.add(ZipWith[Int, Int, Int](math.max _))
+      val zip2 = b.add(ZipWith[Int, Int, Int](math.max _))
+
+      /**
+       *  -->  in0             ---> in0
+       *             zip1.out /         zip2.out ---->
+       *  -->  in1             ---> in1
+       *                      /
+       *  -------------------/
+       */
+      zip1.out ~> zip2.in0
+
+      UniformFanInShape(zip2.out, zip1.in0, zip1.in1, zip2.in1)
+    }
+
+    val resultSink = Sink.head[Int]
+
+    val g = RunnableGraph.fromGraph(GraphDSL.create(resultSink) { implicit b => sink =>
+      import GraphDSL.Implicits._
+
+      // importing the partial graph will return its shape (inlets & outlets)
+      val pm3 = b.add(pickMaxOfThree)
+
+      Source.single(1) ~> pm3.in(0)
+      Source.single(2) ~> pm3.in(1)
+      Source.single(3) ~> pm3.in(2)
+      pm3.out ~> sink.in
+      ClosedShape
+    })
+
+    val max: Future[Int] = g.run()
+    val result = Await.result(max, 300.millis)
+    println(s"finished with ${result}")
+  }
+
+  def testMap(): Unit = {
+    val m = GraphDSL.create() { implicit b =>
+      val map = b.add(Flow[Int].map(x => x*5))
+      FlowShape[Int, Int](map.in, map.out)
+    }
+
+    Source(1 to 5).via(m).runForeach(println(_))
+  }
+
+  def testConstruct(): Unit = {
+    val pairs = Source.fromGraph(GraphDSL.create() { implicit b =>
+      import GraphDSL.Implicits._
+
+      // prepare graph elements
+      val zip = b.add(Zip[Int, Int]())
+      def ints = Source.fromIterator(() => Iterator.from(1))
+
+      // connect the graph
+      ints.filter(_ % 2 != 0) ~> zip.in0
+      ints.filter(_ % 2 == 0) ~> zip.in1
+
+      // expose port
+      SourceShape(zip.out)
+    })
+
+    val firstPair: Future[(Int, Int)] = pairs.runWith(Sink.head)
+    val result = Await.result(firstPair, 1 second)
+    println(s"result = ${result}")
+  }
+
+  def testConcat(): Unit = {
+    val promise = Promise[Int]()
+    val subscriber = TestSubscriber.manualProbe[Int]()
+
+    RunnableGraph.fromGraph(GraphDSL.create() { implicit b ⇒
+      import GraphDSL.Implicits._
+
+      val concat = b add Concat[Int]()
+      Source(List(1, 2, 3)) ~> concat.in(0)
+      Source.fromFuture(promise.future) ~> concat.in(1)
+      concat.out ~> Sink.fromSubscriber(subscriber)
+      ClosedShape
+    }).run()
+
+    val subscription = subscriber.expectSubscription()
+    subscription.request(4)
+
+    /**
+     * expectNext() throws an AssertionError if no element received
+     */
+    subscriber.expectNext()
+    subscriber.expectNext()
+    subscriber.expectNext()
+
+    promise.success(5)
+    subscriber.expectNext()
+  }
+
   def main(args: Array[String]): Unit = {
     try {
       Wrapper("test")(test)
+      Wrapper("testPartial")(testPartial)
+      Wrapper("testMap")(testMap)
+      Wrapper("testConstruct")(testConstruct)
+      Wrapper("testConcat")(testConcat)
     }
     finally {
       println("terminating the system")
