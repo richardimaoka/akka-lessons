7ed12808c0443bcf2eb145bf141dca7e4f3c5e30
Yu <yu.nishiyama@exchange.co.jp>
2017-07-14 11:12:43 +0900

    Formatting SemiGroup

--- src/main/scala/my/cats/SemiGroupApp.scala
+++ src/main/scala/my/cats/SemiGroupApp.scala
@@ -1,34 +1,31 @@
 package my.cats
 
+import my.wrapper.Wrap
+
 
 object SemiGroupApp {
   import cats.implicits._
 
+  def testMap(): Unit = {
+    val a = Map("foo" -> Map("bar" -> 5))
+    val b = Map("foo" -> Map("bar" -> 6), "baz" -> Map[String,Int]())
+    println( a combine b )
+
+    val c = Map("foo" -> List(1, 2))
+    val d = Map("foo" -> List(3, 4), "bar" -> List(42))
+    println( c combine d )
+
+    val e = Map("foo" -> Map("bar" -> 5))
+    val f = Map("foo" -> Map("bar" -> 6), "baz" -> Map())
+    println( e ++ f )
+
+    val g = Map("foo" -> List(1, 2))
+    val h = Map("foo" -> List(3, 4), "bar" -> List(42))
+    println( g ++ h )
+  }
+
   def main(args: Array[String]): Unit ={
-    println(
-      Map("foo" -> Map("bar" -> 5))
-        .combine(
-          Map("foo" -> Map("bar" -> 6), "baz" -> Map())
-        )
-    )
-
-    println(
-      Map("foo" -> List(1, 2))
-        .combine(
-          Map("foo" -> List(3, 4), "bar" -> List(42))
-        )
-    )
-
-    println(
-      Map("foo" -> Map("bar" -> 5))
-        ++ Map("foo" -> Map("bar" -> 6), "baz" -> Map())
-    )
-
-    println(
-      Map("foo" -> List(1, 2))
-        ++ Map("foo" -> List(3, 4), "bar" -> List(42))
-    )
-
+    Wrap("testMap")(testMap)
   }
 
 }
