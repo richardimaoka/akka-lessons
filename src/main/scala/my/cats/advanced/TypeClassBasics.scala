d02f8d6860c47589d21a9c7eb339487968557a52
Richard Imaoka <richard.s.imaoka@gmail.com>
2017-07-25 18:17:32 +0900

    printable

--- src/main/scala/my/cats/advanced/TypeClassBasics.scala
+++ src/main/scala/my/cats/advanced/TypeClassBasics.scala
@@ -1,5 +1,7 @@
 package my.cats.advanced
 
+import my.wrapper.Wrapper
+
 object TypeClassBasics {
 
   def typeClass1(): Unit ={
@@ -31,6 +33,156 @@ object TypeClassBasics {
     /// res5: Json = JsObject(Map(name -> JsString(Dave), email -> JsString(dave@example.com)))
   }
 
+  /*************************************************
+   *  Exercises
+   *************************************************/
+
+  /**
+   * These steps define the three main components of our type class.
+   * First we define Printable—the type class itself:
+   */
+  trait Printable[A] {
+    def format(value: A): String
+  }
+
+  /**
+   * Then we define some ***default*** instances of
+   * Printable and package then in PrintableInstances:
+   */
+  object PrintableInstances {
+    implicit val stringPrintable = new Printable[String] {
+      def format(input: String) = input
+    }
+    implicit val intPrintable = new Printable[Int] {
+      def format(input: Int) = input.toString
+    }
+  }
+
+  def exercise0(): Unit = {
+    import PrintableInstances._ //this is needed!!!
+
+    def printWithFormat[A](a: A)(implicit printable: Printable[A]): Unit = {
+      println(printable.format(a))
+    }
+
+    printWithFormat(1)
+  }
+
+  def exercise1(): Unit = {
+    import PrintableInstances._ //this is needed!!!
+
+    /**
+     * Finally we define an interface object, Printable:
+     */
+    object Printable {
+      def format[A](input: A)(implicit p: Printable[A]): String =
+        p.format(input)
+      def print[A](input: A)(implicit p: Printable[A]): Unit =
+        println(format(input))
+    }
+
+    Printable.format(1)
+    Printable.print(1)
+  }
+
+  def exercise2(): Unit = {
+    object Printable {
+      implicit val stringPrintable = new Printable[String] {
+        def format(input: String) = input
+      }
+      implicit val intPrintable = new Printable[Int] {
+        def format(input: Int) = input.toString
+      }
+      def print[A](a: A)(implicit printable: Printable[A]): Unit = {
+        println(printable.format(a))
+      }
+      def format[A](input: A)(implicit p: Printable[A]): String =
+        p.format(input)
+    }
+
+    final case class Cat(
+      name: String,
+      age: Int,
+      color: String
+    )
+    /**
+     * Then we define type class instances for the types we care about.
+     * These either go into the companion object of Cat or a separate object
+     * to act as a namespace:
+     */
+    object Cat {
+      import Printable._
+      implicit val catPrintable = new Printable[Cat] {
+        def format(cat: Cat): String  = {
+          val name = Printable.format(cat.name)
+          val age = Printable.format(cat.age)
+          val color = Printable.format(cat.color)
+          s"$name is a $age year-old $color cat."
+        }
+      }
+    }
+
+    val cat = Cat("nyatasha", 5, "blue")
+
+    Printable.print(cat)
+  }
+
+  def exercise3(): Unit = {
+    trait Printable[A] {
+      def format(value: A): String
+    }
+
+    object Printable {
+      implicit val stringPrintable = new Printable[String] {
+        def format(input: String) = input
+      }
+      implicit val intPrintable = new Printable[Int] {
+        def format(input: Int) = input.toString
+      }
+      def print[A](a: A)(implicit printable: Printable[A]): Unit = {
+        println(printable.format(a))
+      }
+      def format[A](input: A)(implicit p: Printable[A]): String =
+        p.format(input)
+    }
+
+    /**
+     * First we define an implicit **class** containing our extension methods:
+     */
+    object PrintableSyntax {
+      implicit class PrintOps[A](value: A) {
+        def format(implicit p: Printable[A]): String =
+          p.format(value)
+        def print(implicit p: Printable[A]): Unit =
+          println(p.format(value))
+      }
+    }
+
+    final case class Cat(
+      name: String,
+      age: Int,
+      color: String
+    )
+    object Cat {
+      import Printable._
+      implicit val catPrintable = new Printable[Cat] {
+        def format(cat: Cat): String  = {
+          val name = Printable.format(cat.name)
+          val age = Printable.format(cat.age)
+          val color = Printable.format(cat.color)
+          s"$name is a $age year-old $color cat."
+        }
+      }
+    }
+
+    import PrintableSyntax._
+    Cat("Garfield", 35, "ginger and black").print
+    // Garfield is a 35 year-old ginger and black cat.
+  }
+
   def main(args: Array[String]): Unit = {
+    Wrapper("exercise1")(exercise1)
+    Wrapper("exercise2")(exercise2)
+    Wrapper("exercise3")(exercise3)
   }
 }
