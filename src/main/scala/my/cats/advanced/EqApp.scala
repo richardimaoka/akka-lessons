2e2db928ddf5b4657fd8c28e5cc5e37ba896ec4a
Richard Imaoka <richard.s.imaoka@gmail.com>
2017-07-26 10:25:26 +0900

    Some more on Eq

--- src/main/scala/my/cats/advanced/EqApp.scala
+++ src/main/scala/my/cats/advanced/EqApp.scala
@@ -2,6 +2,62 @@ package my.cats.advanced
 
 object EqApp {
 
+  def basics(): Unit ={
+    import cats.Eq
+    /**
+     * This, not only includes .show() method, but also eq methods too
+     * for the `Int` instance of the Eq type class.
+     */
+    import cats.instances.int._
+    val eqInt = Eq[Int]
+
+    eqInt.eqv(123, 123)
+    // res1: Boolean = true
+
+    eqInt.eqv(123, 234)
+    // res2: Boolean = false
+
+    //This import is needed to enable === and =!= methods below
+    import cats.syntax.eq._
+
+    123 === 123
+    // res4: Boolean = true
+    123 =!= 234
+  }
+
+  def catCompare(): Unit ={
+    import cats.Eq
+    import cats.syntax.eq._
+    import cats.instances.option._ //this is also needed !
+
+    final case class Cat(name: String, age: Int, color: String)
+
+    val cat1 = Cat("Garfield",   35, "orange and black")
+    val cat2 = Cat("Heathcliff", 30, "orange and black")
+
+    val optionCat1 = Option(cat1)
+    val optionCat2 = Option.empty[Cat]
+
+    /**
+     * Another way to define this with Eq.instance[A] method is described later
+     */
+    implicit val catEqual = new Eq[Cat] {
+      import cats.instances.int._
+      import cats.instances.string._
+
+      def eqv(cat1: Cat, cat2: Cat) =
+        (cat1.name  === cat2.name ) &&
+        (cat1.age   === cat2.age  ) &&
+        (cat1.color === cat2.color)
+    }
+
+    /**
+     * See here, you didn't create a type class instance of Eq[Option[Cat]]
+     * but only created Eq[Cat]
+     */
+    optionCat1 === optionCat2
+  }
+
   def main(args: Array[String]): Unit = {
     import cats.Eq
     import cats.syntax.eq._
